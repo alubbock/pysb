8103f2747b4ab41b690b785258e89586e0271f11
Shawn Garbett <shawn@garbett.org>
2013-09-05 23:03:24 -0500

    Some minor fixes

--- pysb/core.py
+++ pysb/core.py
@@ -683,7 +683,8 @@ def as_complex_pattern(v):
     if isinstance(v, ComplexPattern):
         return v
     elif isinstance(v, MonomerPattern):
-        return ComplexPattern([MonomerPattern(v.monomer, v.site_conditions, None)], v.compartment)
+        return ComplexPattern([v], None)
+        #return ComplexPattern([MonomerPattern(v.monomer, v.site_conditions, None)], v.compartment)
     else:
         raise InvalidComplexPatternException
 
